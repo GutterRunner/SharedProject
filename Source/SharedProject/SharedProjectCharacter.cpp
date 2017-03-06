1ae3b1ee8d25450b81f189907872d731cdc2c925
RichSharpy <richsharpy@googlemail.com>
2017-03-06 23:23:12 +0000

    Commit Test
    Noticed my original Commit Test hadn't gone through. Tried resetting
    everything back up and seeing if it will commit through this time.
    
    Comment in the Character.cpp at the top as the main commit test. Other
    two files are automatically created files.

--- Source/SharedProject/SharedProjectCharacter.cpp
+++ Source/SharedProject/SharedProjectCharacter.cpp
@@ -1,5 +1,7 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
+// COMMENT TO TEST COMMITS - Rich
+
 #include "SharedProject.h"
 #include "Kismet/HeadMountedDisplayFunctionLibrary.h"
 #include "SharedProjectCharacter.h"
